7bd7672116fd8073017ba4386891c3ff6f0333ac
Caroline Marques <carolinemcs@hotmail.com>
2022-04-12 22:34:04 -0300

    variaveis

---
+++ variaveis/index.js
@@ -0,0 +1,22 @@
+// Exercícios de interpretação de código
+
+// Exercício 1
+/*let a = 10
+let b = 10
+console.log (b)*/
+resposta 10
+/*b =5
+console.log (a,b)*/
+resposta 10,10
+
+
+// Exercício 2
+resposta console.log (10, 10, 10)
+
+// Exercício 3
+let horasTrabalhadasDia = prompt ()
+let valorDia = prompt ()
+alert valorHora
+
+
+// Exercícios de escrita de código
