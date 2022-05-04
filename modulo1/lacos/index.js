da452425c4e1e6efccaf2a80159e8dfd40a5788a
Caroline Marques <carolinemcs@hotmail.com>
2022-05-03 22:32:27 -0300

    exercícios de laços

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,111 @@
+// EXERCÍCIOS DE LAÇOS
+
+// EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO
+
+/*
+Exercício 1 
+O uso de Laços é uma forma de fazer o algoritmo repetir uma ação em loops. 
+Neste caso, o valor impresso no console será definido a partir da soma
+dos indices no array menores que 5, ou seja de 0 a 4.
+O valor da variável começa em 0, e vai aumentando somando sempre com o índice anterior,
+a partir da especificação dada no incremento (i++),
+o valor impresso será 10.
+  
+
+Exercício 2 
+a) Serão impressos os número maiores que 18, ou seja, a partir do número 19.
+
+b) 
+Sim. 
+Para realizar esta ação seria necessário completar o bloco com a seguinte ação:
+
+for (let indiceArray for lista){
+
+}
+o for...of funciona com arrays, e tem como finalidade varrer cada posição do array.
+
+
+Exercício 3
+
+Serão impressos de 1 a 4, cada um correspondente a uma linha: 
+*
+**
+***
+****
+
+*/
+
+// EXERCÍCIOS DE ESCRITA DE CÓDIGO
+
+
+// Exercício 1
+
+// let numeroBichoEstimacao = Number(prompt("Quantos bichinhos de estimação você tem?"));
+// let arrayBicho = []
+
+//     if (numeroBichoEstimacao === 0) {
+    
+//         console.log ("Que pena! Você pode adotar um pet!")
+//     }
+//     else {
+        
+//         for (bicho=0; bicho<numeroBichoEstimacao; bicho++) {
+//             let nomeBichos = prompt("Digite o nome do seu bichinho de estimação.")
+           
+//             arrayBicho.push(nomeBichos)
+                   
+//         }
+//      }
+
+//      console.log (arrayBicho)
+ 
+
+// Exercício 2
+// a)
+// let arrayOriginal = [80,30,130,40,60,21,70,120,90,103,110,55]
+
+// function imprimirNumeros (array) {
+//    for (let numerosDoArray of arrayOriginal){
+   
+//     }
+      
+// } 
+
+// console.log (arrayOriginal)
+
+
+
+
+
+// let arrayOriginal = [80,30,130,40,60,21,70,120,90,103,110,55]
+
+// // b)
+
+// function imprimirNumeros (array) {
+      
+//     for (let i = 0; i < array.length; i++) {
+   
+//         return (array[i/10])
+//     }
+      
+// } 
+
+// imprimirNumeros(array)
+
+
+
+
+
+
+//     console.log(arrayOriginal)
+// }
+ 
+// let i=0
+// function imprimirCadaPorDez () {
+
+//     while (i/10 === % 0) {
+
+
+//     }
+
+// }
