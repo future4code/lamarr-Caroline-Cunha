17e9ab4bff770ac2cdb8eebdcc7089d2f74e5e90
Caroline Marques <carolinemcs@hotmail.com>
2022-05-23 12:53:13 -0300

    Refazendo Exercícios de Variáveis

--- variaveis/index.js
+++ variaveis/index.js
@@ -1,5 +1,113 @@
 // Exercícios de interpretação de código
 
+
+// Refazendo Exercícios
+
+
+/*
+
+Exercício 1
+
+5
+10,5
+
+
+Exercício 2
+
+10, 10, 10
+
+
+Exercício 3
+
+let horasTrabDia = prompt ("Quantas horas você trabalha por dia?")
+let valorPorDia = prompt ("Quanto você recebe por dia?")
+console.log (`Você recebe ${valorPorDia/horasTrabDia} por hora.`)
+
+*/
+
+
+
+// Exercícios de Escrita de código
+
+// Exercício 1
+
+// let nome
+// let idade
+
+// console.log (typeof nome)
+// console.log (typeof idade)
+
+// A mensagem impressa foi undefined porque nenhum valor foi especificado para as variáveis.
+
+// let nome = prompt ("Digite seu nome.")
+// let idade = prompt ("Digite sua idade.")
+
+// console.log (typeof nome)
+// console.log (typeof idade)
+
+// // Apesar de a idade ser um número, veio impressa no console como uma string. Para alterar, seria necessário converter para número com a expressão Number.
+// console.log (nome, idade)
+// console.log (`Olá ${nome}, você tem ${idade} anos.`)
+
+// Exercício 2
+
+// let roupaAzul = prompt ("Você está usando roupa azul?")
+// let comidaPreferida = prompt ("Brócolis é sua comida favorita?")
+// let aniversario = prompt ("Você faz aniversário hoje?")
+
+
+// console.log ("Você está usando roupa azul?", roupaAzul)
+// console.log ("Brócolis é sua comida favorita?", comidaPreferida)
+// console.log ("Você faz aniversário hoje?", aniversario)
+
+
+
+// Exercício 3
+
+// let a = 10
+// let b = 25
+
+// c = a
+// a = b
+// b = c
+
+
+// console.log (a, b)
+
+
+// DESAFIO
+
+let num1 = Number(prompt("Digite um número inteiro."))
+let num2 = Number(prompt("Digite outro número inteiro."))
+
+let x = num1 + num2
+let y = num1 * num2
+
+console.log (x, y)
+
+
+
+
+
+
+// let a = 10
+// let b = 10
+// b = 5
+
+// console.log (a,b)
+
+
+// let a = 10
+// let b = 20
+// c = a
+// b = c
+// a = b
+
+// console.log (a,b,c)
+
+
+// 
+
 // Exercício 1
 /*let a = 10
 let b = 10
@@ -13,14 +121,14 @@ console.log (a,b)*/
 
 // Exercício 2
 
-console.log (a, b, c)
-// resposta 10, 10, 10
+// console.log (a, b, c)
+// // resposta 10, 10, 10
 
-// Exercício 3
+// // Exercício 3
 
-let horasTrabalhadasDia
-let valorDia
-/*alert*/ valorHora
+// let horasTrabalhadasDia
+// let valorDia
+// /*alert*/ valorHora
 
 
 // Exercícios de escrita de código
@@ -30,31 +138,31 @@ let valorDia
 // let nome
 // let idade
 
-console.log (typeof nome)
-console.log (typeof idade)
+// console.log (typeof nome)
+// console.log (typeof idade)
 
-/* Esse tipo foi impresso porque não há predefinição da informação, ou seja, 
-definição dos valores das variáveis*/
+// /* Esse tipo foi impresso porque não há predefinição da informação, ou seja, 
+// definição dos valores das variáveis*/
 
-const nome = prompt (typeof nome)
-let idade = prompt (typeof idade)
-console.log (prompt ("Qual seu", nome, "?", "Qual sua", idade, "?"))
+// const nome = prompt (typeof nome)
+// let idade = prompt (typeof idade)
+// console.log (prompt ("Qual seu", nome, "?", "Qual sua", idade, "?"))
 
-console.log ("Olá!", nome, "Você tem", idade, "anos", ".")
+// console.log ("Olá!", nome, "Você tem", idade, "anos", ".")
 
-// Exercício 2
+// // Exercício 2
 
 //  pergunta1 - Você gosta de chocolate? - Sim
 //  pergunta2 - Você canta no banheiro? - Sim
 //  pergunta3 - Branco traz paz? - Sim
 
-const pergunta1 = prompt ("Você gosta de chocolate?")
-const pergunta2 = prompt ("Você canta no banheiro?")
-const pergunta3 = prompt ("Branco traz paz?")
+// const pergunta1 = prompt ("Você gosta de chocolate?")
+// const pergunta2 = prompt ("Você canta no banheiro?")
+// const pergunta3 = prompt ("Branco traz paz?")
 
-console.log ("Você gosta de chocolate?", pergunta1)
-console.log ("Você canta no banheiro?", pergunta2)
-console.log ("Branco traz paz?", pergunta3)
+// console.log ("Você gosta de chocolate?", pergunta1)
+// console.log ("Você canta no banheiro?", pergunta2)
+// console.log ("Branco traz paz?", pergunta3)
 
-// Exercício 3
+// // Exercício 3
 
