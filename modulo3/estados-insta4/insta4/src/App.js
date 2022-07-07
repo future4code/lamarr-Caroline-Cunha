8b250a13656169f59a5d8283cd5d0304965fc589
Caroline Marques <carolinemcs@hotmail.com>
2022-07-07 20:33:51 -0300

    inclusão de informações

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+// import React from 'react';
 import Post from './components/Post/Post';
 import './style.css';
 import styled from 'styled-components';
-
+import React, {useState} from 'react';
 
 
 const MainCont = styled.div`
@@ -10,11 +10,16 @@ const MainCont = styled.div`
     justify-content: center;
     flex-direction: column;
     align-items: center
-
 `
+
 
 function App() {
 
+  const [inputNomeUsuario, setInputNomeUsuario] = useState ("")
+  const [inputFotoUsuario, setInputFotoUsuario] = useState ("")
+  const [inputFotoPost, setInputFotoPost] = useState ("")
+  
+  
   const arrayPost = [
 
     {nomeUsuario:"Paulinha", 
@@ -30,7 +35,23 @@ function App() {
      fotoPost:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRohChuhjVvudXlkG0dHf1steXY-bX_J7mZA&usqp=CAU",
     }
   ]
-  
+
+  const handleInputNomeUsuario = (e) => {
+    e.target.value
+  }
+
+  const handleInputFotoUsuario = (e) => {
+    e.target.value
+  }
+
+  const handleInputFotoPost = (e) => {
+    e.target.value
+  }
+
+  const adicionarPost = (e) => {
+    e.preventDefault();
+  }
+
     const listaPost=arrayPost.map((elemento, index)=>{
    
       return  <Post 
@@ -43,18 +64,56 @@ function App() {
    
   })
 
-  return (
-    <MainCont>
-      {listaPost}
+  function Posts () {
+    const [listaPost, setPosts] = useState ([
+    {nomeUsuario:"Paulinha", 
+     fotoUsuario:"https://picsum.photos/50/50",
+     fotoPost:"https://picsum.photos/200/150",
+    },
+    {nomeUsuario:"Lia", 
+     fotoUsuario:"https://vignette.wikia.nocookie.net/clubpenguin/images/1/16/Yellow_Snorkel.PNG/revision/latest?cb=20130322161821", 
+     fotoPost:"http://ecoviagem.com.br/noticias/turismo/turismo-internacional/cinco-atividades-para-aprender-em-uma-viagem-a-aruba-18784/centro-82794.jpg",
+    },
+    {nomeUsuario:"Mila", 
+     fotoUsuario:"https://apuamarafting.com.br/wp-content/uploads/2018/03/rapel.png", 
+     fotoPost:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRohChuhjVvudXlkG0dHf1steXY-bX_J7mZA&usqp=CAU",
+    }])
+      
+    
+    const adicionarPost = () => {
+      const novoPost = {nomeUsuario:inputNomeUsuario, fotoUsuario: inputFotoUsuario, fotoPost: inputFotoPost}
+      const adicionarNovoPost = [...listaPost, novoPost]
+      setListaPost(adicionarNovoPost)
 
-      {console.log(arrayPost)}
+    }
+    return (
 
-    </MainCont>
+      {novoPost}
+    )}
 
-  )
 }
 
-
-
-
 export default App;
+
+//    const adicionarNomeUsuario = () => {
+
+    //         const novoNomeUsuario = [...listaPost]
+    //         novoPost.push(handleInputNomeUsuario)
+    //         setPosts(novoPost)
+    // }
+    
+
+    // const adicionarFotoUsuario = () => {
+
+    //   const novaFotousuario = [...listaPost]
+    //   novoPost.push(handleInputFotoUsuario)
+    //   setPosts(novoPost)
+    // }
+
+
+    // const adicionarFotoPost = () => {
+
+    //   const novoNome = [...listaPost]
+    //   novoPost.push(inputNomeUsuario)
+    //   setPosts(novoPost)
+    // }
