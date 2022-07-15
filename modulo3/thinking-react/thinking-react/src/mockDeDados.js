d99ff9303d55604afc643f69b68b9253c259d99b
Caroline Marques <carolinemcs@hotmail.com>
2022-07-14 22:02:08 -0300

    Projeto Thinking-react

---
+++ modulo3/thinking-react/thinking-react/src/mockDeDados.js
@@ -0,0 +1,10 @@
+
+export const produtos = [
+    {id:"", nome:"", valor: "", imagem:""},
+    {id:"", nome:"", valor: "", imagem:""},
+    {id:"", nome:"", valor: "", imagem:""},
+    {id:"", nome:"", valor: "", imagem:""},
+    {id:"", nome:"", valor: "", imagem:""},
+    {id:"", nome:"", valor: "", imagem:""},
+    
+]
