6d193531e60400e7d70c95d0ffb08f05ccb63341
Caroline Marques <carolinemcs@hotmail.com>
2022-08-05 21:50:26 -0300

    Tentativa de chamar API

--- modulo3/projetoastromach/projetoastromach/src/Components/Pages/Homepage.js
+++ modulo3/projetoastromach/projetoastromach/src/Components/Pages/Homepage.js
@@ -3,34 +3,47 @@ import Card from "../Card/Card";
 import styled from "styled-components";
 import axios from 'axios';
 
+
 const HomepageContainer = styled.main`
     display: flex;
     flex-wrap: wrap;
 `
 
 const Homepage = () => {
+    
+    const getProfileToChoose = () => {
+        axios.get ("https://us-central1-missao-newton.cloudfunctions.net/astroMatch/carol/person")
+        .then((response)=> console.log(response.data))
+        .catch((error) => console.log(error.message.data))
+            
+    }
+        
     const [myList, setMyList] = useState ([]);
-    
+    const componentLista=myList.map((item, index)=>{
 
-    
-    return (
+        return (
             <HomepageContainer>
                 <Card
-                    id={''}
-                    age={'36 anos'}
-                    name={'Cesar'}
-                    photo={''}
-                    bio={'lindo'}
+                    id={item.id}
+                    age={item.age}
+                    name={item.name}
+                    photo={item.photo}
+                    bio={item.bio}
                 />
             </HomepageContainer>
         )
+    })
+
+    useEffect(() => {
+        getProfileToChoose()
+    }, [] 
+    )
+     
+     return(
+        <div>
+            {componentLista}
+        </div>
+    )
 }
 
 export default Homepage;
-
-
-
-    // const componentLista=myList.map((item, index)=>{
-  
-    // const url='https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:aluno/person'
-    // const 
